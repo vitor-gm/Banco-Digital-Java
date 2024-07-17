203b54abb720ffbdd06cd07efab946946f59eea8
vitor-gm <gm.0509rbt@gmail.com>
2024-07-17 12:09:24 -0300

    Adição da classe Extrato

--- src/main/java/com/exemplo/BancoDigital/Main/Main.java
+++ src/main/java/com/exemplo/BancoDigital/Main/Main.java
@@ -1,30 +1,26 @@
 package main.java.com.exemplo.BancoDigital.Main;
 
-import main.java.com.exemplo.BancoDigital.model.Banco;
-import main.java.com.exemplo.BancoDigital.model.Cliente;
-import main.java.com.exemplo.BancoDigital.model.ContaSilver;
-import main.java.com.exemplo.BancoDigital.model.Endereco;
+import main.java.com.exemplo.BancoDigital.model.*;
 import main.java.com.exemplo.BancoDigital.service.Conta;
 import main.java.com.exemplo.BancoDigital.service.ServicoConta;
+
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco("F Bank", 9050, new Endereco("Rio de Janeiro", "Madureira", 300), 1000000d);
         Conta conta = new Conta(1458, 0001, 5d, 1235, 0d, banco);
         Conta conta2 = new Conta(9857, 0001, 7d, 7894, 0d, banco);
-
-
-
-
-
         ContaSilver contaSilver = new ContaSilver(002, 001, 10d, 4321, 0d, banco);
+
+
         banco.getClientes().add(new Cliente("João Carlos", new Endereco("Rio de Janeiro", "28986348", 251), 2516448, conta));
         banco.getClientes().add(new Cliente("Pedro Silva", new Endereco("Rio de Janeiro", "28568352", 255), 2539848, contaSilver));
-        banco.getClientes().add(new Cliente("Julianna Alves", new Endereco("Rio de Janeiro", "28986367", 250), 2853548, conta));
-        banco.getClientes().forEach(n -> {
-            System.out.println(n.getNome());
-            System.out.println(n.getConta().getSaldo());
-        });
+        banco.getClientes().add(new Cliente("Julianna Alves", new Endereco("Rio de Janeiro", "28986367", 250), 2853548, conta2));
+
+        conta.depositar(100d);
+
+
 
 
 
--- src/main/java/com/exemplo/BancoDigital/model/Extrato.java
+++ src/main/java/com/exemplo/BancoDigital/model/Extrato.java
@@ -5,4 +5,31 @@ public class Extrato {
     private String operacao;
     private double saldo;
     private double valor;
+
+    public Extrato(String operacao, double saldo, double valor) {
+        this.operacao = operacao;
+        this.saldo = saldo;
+        this.valor = valor;
+    }
+
+    public String getOperacao() {
+        return operacao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Extrato{" +
+                "operacao='" + operacao + '\'' +
+                ", saldo=" + saldo +
+                ", valor=" + valor +
+                '}';
+    }
 }
